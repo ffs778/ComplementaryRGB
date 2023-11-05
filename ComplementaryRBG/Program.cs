1cd6660b762a1331d77e3ac16d796c6eff380291
王海平 <2530611886@qq.com>
2023-11-05 15:34:02 +0800

    测试从仓库撤销已提交的合并代码

--- ComplementaryRBG/Program.cs
+++ ComplementaryRBG/Program.cs
@@ -18,6 +18,8 @@ namespace ComplementaryRGB
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+            // test
         }
     }
 }
