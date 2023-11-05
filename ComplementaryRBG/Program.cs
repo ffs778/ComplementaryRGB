6059bd5f42ea0ec3a84d226d656133830d910c52
王海平 <2530611886@qq.com>
2023-11-05 15:35:23 +0800

    Revert "测试从仓库撤销已提交的合并代码"
    This reverts commit 1cd6660b762a1331d77e3ac16d796c6eff380291.

--- ComplementaryRBG/Program.cs
+++ ComplementaryRBG/Program.cs
@@ -18,8 +18,6 @@ namespace ComplementaryRGB
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
-
-            // test
         }
     }
 }
